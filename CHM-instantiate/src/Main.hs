9caf4fc0d0f770938e0f178e1ae2d7624a6b195b
jiriklepl <jiriklepl@seznam.cz>
2020-03-07 14:15:11 +0100

    reformated Main.hs

--- CHM-instantiate/src/Main.hs
+++ CHM-instantiate/src/Main.hs
@@ -1,16 +1,17 @@
 -- copied from https://github.com/visq/language-c
 module Main where
-    import Language.C
-    import Language.C.System.GCC
 
-    main = parseMyFile "test.chm" >>= printMyAST
+import Language.C
+import Language.C.System.GCC
 
-    parseMyFile :: FilePath -> IO CTranslUnit
-    parseMyFile input_file =
-      do parse_result <- parseCFile (newGCC "gcc") Nothing [] input_file
-         case parse_result of
-           Left parse_err -> error (show parse_err)
-           Right ast      -> return ast
+main = parseMyFile "test.chm" >>= printMyAST
 
-    printMyAST :: CTranslUnit -> IO ()
-    printMyAST = print
+parseMyFile :: FilePath -> IO CTranslUnit
+parseMyFile input_file =
+  do parse_result <- parseCFile (newGCC "gcc") Nothing [] input_file
+     case parse_result of
+       Left parse_err -> error (show parse_err)
+       Right ast      -> return ast
+
+printMyAST :: CTranslUnit -> IO ()
+printMyAST = print . pretty
