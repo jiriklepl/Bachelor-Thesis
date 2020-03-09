f2810c7053cfaccc2630ba8bec74966c26dc4eda
jiriklepl <jiriklepl@seznam.cz>
2020-03-09 18:38:40 +0100

    partly done Transform CExpr (+ some minor changes)

--- AST-to-THIH/src/CHM/Transform.hs
+++ AST-to-THIH/src/CHM/Transform.hs
@@ -34,13 +34,14 @@ instance FindReturn CStat where
     CCases _ _ a _ -> findReturn a
     CDefault a _ -> findReturn a
     CCompound _ block _ ->
-      foldl (++) [] [findReturn x | x <- block] -- TODO
+      foldl (++) [] [findReturn x | x <- block]  -- TODO
     CIf _ a (Just b) _ -> findReturn a ++ findReturn b
     CIf _ a (Nothing) _ -> findReturn a
     CSwitch _ a _ -> findReturn a
     CWhile _ a _ _ -> findReturn a
     CFor _ _ _ a _ -> findReturn a
-    CReturn _ _ -> -- finally
+    CReturn (Nothing) _ -> [Var "()"]
+    CReturn (Just a) _ ->
     CExpr _ _ -> []
     CGoto _ _ -> []
     CGotoPtr _ _ -> []
@@ -49,9 +50,59 @@ instance FindReturn CStat where
     CAsm _ _ -> []
 
 instance FindReturn CBlockItem where
-  findReturn CBlockStmt -- TODO:
-  findReturn CBlockDecl -- TODO:
-  findReturn CNestedFunDef -- TODO:
+  findReturn CBlockStmt  -- TODO:
+  findReturn CBlockDecl  -- TODO:
+  findReturn CNestedFunDef  -- TODO:
+
+class OperatorFunction a where
+  operatorFunction :: a -> Id
+
+ternaryOpFunc :: Id
+elvisOpFunc :: Id
+indexOpFunc :: Id
+
+instance Transform CExpr where
+  -- the top-most binding should be first recursively (in comparison that would be the binding of ==, then operands and then their child bindings)
+  transform cExpr = case CExpr of
+    -- exprs is guaranteed to have at least 2 elements
+    CComma exprs _ ->
+      -- the very last (return-like) expression in the comma-separated list
+      transform (last exprs) ++
+      -- others
+      foldl (++) [] [transform x | x <- init exprs]
+    CAssign op lExpr rExpr _ ->
+      [([],[[Ap $ Ap (Var $ operatorFunction op) lExpr rExpr]])] ++
+      transform lExpr ++ -- should be l-expression (todo)
+      transform rExpr
+    -- this is the ternary operator
+    CCond cExpr (Just tExpr) fExpr _ ->
+      [([],[[Ap $ Ap (Var ternaryOpFunc) tExpr fExpr]])] ++
+      transform tExpr ++
+      transform eExpr
+    -- this is elvis (supported by gnu)
+    CCond cExpr (Nothing) fExpr ->
+      [([],[[Ap (Var trinaryOpFunc) fExpr]])] ++
+      transform fExpr
+    CBinary op lExpr rExpr ->
+      [([],[[Ap $ Ap (Var $ operatorFunction op) lExpr rExpr]])] ++
+      transform lExpr ++
+      transform rExpr
+    -- TODO: CCast
+    CUnary op expr ->
+      [([],[[Ap (Var $ operatorFunction op) expr]])] ++
+      transform expr
+    -- TODO: CSizeofExpr
+    -- TODO: CSizeofType
+    -- ditto align
+    -- TODO: CComplexReal
+    CIndex aExpr iExpr ->
+      [([],[[Ap $ Ap (Var indexOpFunc) lExpr rExpr]])] ++
+      transform aExpr ++
+      transform iExpr
+    -- TODO
+
+
+
 
 instance Transform CFunDef where
   transform (CFunDef specs decl decls stmt _) =
